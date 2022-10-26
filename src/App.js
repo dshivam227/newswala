971da6134527430a8516855788e3de6e75822cf6
Shivam Dubey <dshivam227@gmail.com>
2022-10-26 12:06:33 +0530

    initalising repo

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Navbar from './components/Navbar';
 import News from './components/News';
 import LoadingBar from 'react-top-loading-bar'
 import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+import Slider from './components/Slider';
 
 const App = () => {
 
   return (
+          
     <div>
+   
       <Router>
         <Navbar />
         <LoadingBar
@@ -18,6 +21,7 @@ const App = () => {
         // onLoaderFinished={() => setProgress(0)}
         />
         <Routes>
+   
           <Route exact path="/" element={<News key="general" country='in' category='general' />} />
           <Route exact path="/business" element={<News key="business" country='in' category='business' />} />
           <Route exact path="/entertainment" element={<News key="entertainment" country='in' category='entertainment' />} />
@@ -27,7 +31,7 @@ const App = () => {
           <Route exact path="/sports" element={<News key="sports" country='in' category='sports' />} />
           <Route exact path="/technology" element={<News key="technology" country='in' category='technology' />} />
         </Routes>
-
+    
       </Router>
     </div>
 
